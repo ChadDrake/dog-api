432d57c4bfa4c6f4d299b63070404a608a231c3e
Chad Drake <chaddrake1313@yahoo.com>
2020-08-17 11:28:34 -0500

    progress

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import $ from 'jquery';
 
-function getNumberOfDoggos(){
-  let number = $('.number').val()
-$('.number').val('');
-  console.log(number);
-};
-
-function handleSumbit() {
-  $('main').on('submit'){
-getNumberOfDoggos
-  }
-};
-
-function main() {
-  
+function getNumberOfDoggos() {
+  let number = $('input[name=number]').val();
+  $('input[name=number]').val('3');
+  return number;
 }
 
+function getImages() {
+  //let result =[];
+  fetch(`https://dog.ceo/api/breeds/image/random/${getNumberOfDoggos()}`
+  )
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+}
+function handleSumbit() {
+  $('main').on('submit', function (e) {
+    e.preventDefault();
+    getImages();
+    
+  });
+}
+
+function main() {
+  handleSumbit();
+}
 $(main);
